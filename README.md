29126d1de50d4e2b45c7e2ba939c942fa0d50453
Joris Slob <j.slob@topdesk.com>
2025-06-06 16:55:51 +0200

    first prototype of a world with water

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# tins2025
+# Food Planet (TINS 2025)
+
 My contribution for TINS 2025 (https://tins.amarillion.org/news/)
+
+Food Planet is a game about food webs. You start with a barren planet. Along
+the way you get more tools to populate your planet, but be careful to maintain
+the balance.
+
+Different organisms eat each other, so it is good to watch closely how they 
+interact. One of your new organisms could be a preditor or prey in the new
+balance. This **food** web is the core of the game.
+
+Technically, the game is implemented by going through different **layers**
+of simulation. These are done in order and are rendered to the screen in
+order, where more primitive organisms are at the bottom and more advanced
+at the top.
+
+To place different organisms, you first have to select the tool from the
+**cute** buttons on the right (#TODO).
+
+Can you find the **golden ratio** that keeps most types of organisms alive
+in a steady equilibrium?
--- tins2025js.js
+++ tins2025js.js
@@ -1,41 +1,94 @@
 const Gamestate = Object.freeze({
   Title: Symbol("title"),
   Game: Symbol("game"),
-  Gameover: Symbol("gameover")
+});
+
+const Tool = Object.freeze({
+  Grass: Symbol("grass")
 });
 
 let current_state = Gamestate.Title;
-let counter = 0;
+let tool = Tool.Grass;
+let img_title;
+let waterDrops = [];
 
 function setup() {
-  createCanvas(800,600);
+  cnv = createCanvas(800,600);
+  let newCanvasX = (windowWidth - 800)/2;
+  let newCanvasY = (windowHeight - 600)/2;
+  cnv.position(newCanvasX, newCanvasY);
+}
+
+function preload() {
+  img_title = loadImage('/assets/title.png');
 }
 
 
 function draw() {
-  console.log("hello world");
   switch (current_state) {
     case Gamestate.Title:
-      background(255,0,0);
-      if (counter > 100) {
-        current_state = Gamestate.Game;
-        counter = 0;
-      }
+      title();
       break;
     case Gamestate.Game:
-      background(0,255,0);
-      if (counter > 100) {
-        current_state = Gamestate.Gameover;
-        counter = 0;
-      }
-      break;
-    case Gamestate.Gameover:
-      background(0,0,255);
-      if (counter > 100) {
-        current_state = Gamestate.Title;
-        counter = 0;
-      }
+      gameloop();
       break;
   }
-  counter++;
+}
+
+function title() {
+  background(0,0,0);
+  image(img_title, 0, 0);
+  if (mouseIsPressed === true) {
+    current_state = Gamestate.Game;
+  }
+}
+
+function gameloop() {
+  background(0,0,0);
+  fill(139,69,19);
+  // Layer one, bedrock
+  circle(300,300,300);
+  // Layer two, water
+  drawWater();
+}
+
+function drawWater() {
+  waterDrops = waterDrops.filter(drop => drop.d != 1);
+  for (let i = 0; i < waterDrops.length; i++) {
+    let drop = waterDrops[i];
+    for (let j = 0; j < waterDrops.length; j++) {
+      if (i != j) {
+        let other = waterDrops[j];
+        let d = dist(drop.x, drop.y, other.x, other.y);
+        if (d < (drop.d + other.d)/2) {
+          other.d += drop.d;
+          drop.d = 1;
+        } else {      
+          drop.x += (other.x - drop.x) / d**2;
+          drop.y += (other.y - drop.y) / d**2;
+        }
+      }
+    }
+    fill(0, 100, 255, 150);
+    ellipse(drop.x, drop.y, drop.d, drop.d);
+  }
+}
+
+function mouseClicked(event) {
+  if (current_state == Gamestate.Title) {
+    current_state = Gamestate.Game;
+  } else {
+    // We are in the game!
+    if (mouseX >= 600) {
+      console.log("select tool!");
+    }
+    else {
+      console.log("use tool!");
+      d_center = dist(mouseX, mouseY, 300, 300);
+      if (d_center < 150) {
+        waterDrops.push({ x: mouseX, y: mouseY, d: 2 });
+        console.log(waterDrops.length);
+      }
+    }
+  } 
 }
